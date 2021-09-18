e5010aa3819227cc09b67b144e97735057d7f48b
andreas <andsek@hotmail.com>
2021-09-18 22:15:56 +0200

    another commit

---
+++ src/main/java/com/andynordevelop/miraiexwrapper/MiraiexwrapperApplication.java
@@ -0,0 +1,13 @@
+package com.andynordevelop.miraiexwrapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiraiexwrapperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiraiexwrapperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andynordevelop/miraiexwrapper/MiraiexwrapperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andynordevelop.miraiexwrapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiraiexwrapperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
